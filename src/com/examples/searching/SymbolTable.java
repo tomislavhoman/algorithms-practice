830dba73a3df6cd310f3fc4bda858d41c60b7bc9
Tomislav Homan <tomislav.homan@fiveminutes.eu>
2016-10-12 20:43:35 +0200

    Implement sequential symbol table and test suite for symbol tables

---
+++ src/com/examples/searching/SymbolTable.java
@@ -0,0 +1,16 @@
+package com.examples.searching;
+
+public interface SymbolTable<Key, Value> {
+
+    Value get(Key key);
+
+    void put(Key key, Value value);
+
+    void delete(Key key);
+
+    boolean contains(Key key);
+
+    boolean isEmpty();
+
+    int size();
+}
